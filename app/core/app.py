b43d5a97c5c5f29acb00c707453f4920d026a86a
JPVT <akkachik@gmail.com>
2025-06-06 20:41:52 +0200

    up render 1

--- app/core/app.py
+++ app/core/app.py
@@ -146,4 +146,5 @@ def afficher_matchs(path):
 # ▶️ Lancement local
 # ======================================
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
