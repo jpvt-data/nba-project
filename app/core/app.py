fcb4826156499cd6e9079e2cff21569f130c3505
JPVT <akkachik@gmail.com>
2025-06-17 00:38:00 +0200

    up render

--- app/core/app.py
+++ app/core/app.py
@@ -300,4 +300,4 @@ def afficher_avatar_utilisateur(session, pathname):
 # ===============================
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
